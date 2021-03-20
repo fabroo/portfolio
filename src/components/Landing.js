f387418af0ce42ba84041714e61a03a073c70330
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2021-03-20 19:13:25 -0300

    version 2

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
 
 export default class Landing extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            count:0
+            count: 0,
+            show:false
         }
     }
     render() {
-        const increase = (e) =>{
+        const increase = (e) => {
             e.preventDefault();
-            this.setState({count:this.state.count + 1})
+            this.setState({ count: this.state.count + 1 })
         }
         return (
             <div className="container align">
-                <div className="main-container align">
-                    <h1>hola</h1>
+            <AnimatePresence>
+               {this.state.show &&  <motion.div
+                    className="main-container align"
+                    initial={{ scale: 0 }}
+                    key="modal"
+                    exit={{scale:0,opacity:0}}
+                    animate={{ rotate: 360, scale: 1 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 70,
+                        damping: 10
+                    }}
+                >
+                    <p className="main-txt">hola!</p>
                     <button onClick={(e) => increase(e)}>apretame</button>
-                    {this.state.count > 0 ? `vas ${this.state.count}` : null}
-                    <h2 onClick={() => alert(`LOL ${this.state.count} toques`)}>apretame</h2>
-                </div>
+                    {this.state.count > 0 ? <p>vas {this.state.count}</p> : null}
+                    <p onClick={() => alert(`LOL ${this.state.count} toques`)}><b>apretame</b></p>
+                </motion.div>}
+            </AnimatePresence>
+
+               {!this.state.show &&  <button onClick={(e) => {
+                    e.preventDefault();
+                    this.setState({show:!this.state.show})
+                }}>Hola</button>}
+
             </div>
         )
     }
